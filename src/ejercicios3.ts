53c97dcc2343a10b365705e8d1e55936a62bb63c
luchinileo <104324324+luchinileo@users.noreply.github.com>
2022-04-24 19:05:44 -0300

    ejercicios3.ts

---
+++ src/ejercicios3.ts
@@ -0,0 +1,8 @@
+console.log("girar ala izquierda");
+console.log("avanzar");
+console.log("girar ala derecha");
+console.log("avanzar");
+console.log("avanzar");
+console.log("girar ala derecha");
+console.log("avanzar");
+console.log("eliminar");
